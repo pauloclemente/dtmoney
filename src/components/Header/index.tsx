fa1f70f3bf9df36132f24d89db4ac125f9e03951
Paulo Clemente <clementedbarros@gmail.com>
2022-03-05 23:36:02 -0300

    chore: config lint

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,30 @@
-import logoImg from '../../assets/logoSvg.svg'
-import { Container, Content } from './styles'
+import { useState } from 'react';
+import Modal from 'react-modal';
+import logoImg from '../../assets/logoSvg.svg';
+import { Container, Content } from './styles';
 
-export function Header () {
+export function Header() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(
+    false,
+  );
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <Container>
-        <Content>
-            <img src={logoImg} alt="dt money"/>
-            <button type="button">
-                Nova transação
-            </button>
-        </Content>
+      <Content>
+        <img src={logoImg} alt="dt money" />
+        <button type="button">Nova transação</button>
+        <Modal isOpen={isNewTransactionModalOpen}>
+          <h2>Cadastrar transação</h2>
+        </Modal>
+      </Content>
     </Container>
-  )
+  );
 }
